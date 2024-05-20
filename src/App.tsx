0451b2c091fc8dac9a3c0a1eed3e2f894a9890e6
Herlian <herlianzhang370@gmail.com>
2024-05-20 11:52:52 +0700

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@/components/ui/input";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl">Hello World</h1>
+      <Input type="email" placeholder="Email" />
+    </>
+  );
+}
+
+export default App;
